b1f28dae111a36bf01751c11d4ebafa2d6ebf585
Daniween <soudry.daniel99@gmail.com>
2026-03-10 17:03:35 +0100

    text

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -133,7 +133,6 @@ watch(() => props.user, (newUser) => {
       <!-- LEADERBOARD TRIGGER -->
       <button @click="$emit('show-leaderboard')" class="menu-btn tertiary ranking-btn">
         Afficher le classement mondial
-        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20 20H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2z"></path><path d="M12 16l4-4-4-4"></path><line x1="8" y1="12" x2="16" y2="12"></line></svg>
       </button>
     </div>
 
